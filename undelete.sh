bf976022cd90f5e409caf585d8da395ccf55716c
Jonas Posselt <jonas.posselt@web.de>
2018-05-09 11:32:17 +0200

    Aufgabe 3 WIP

---
+++ undelete.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Shell Script zum Wiederherlstellen von Dateien aus dem Papierkorb
+
+TRASHDIR="$HOME/.trashBin"
+TRASHLOG="$TRASHDIR/.dir"
+
+# Parameter prüfen
+if [ -z "$1" ]
+then
+    echo "Usage: $0 [FILE]..."
+    exit 1
+fi
+
+# Papierkorb existiert?
+if [ ! -e $TRASHDIR ]
+then
+    echo "Papierkorbverzeichnis nicht gefunden"
+    exit 1
+fi
+
+# Logdatei existiert?
+if [ ! -e $TRASHLOG ]
+then
+    echo "Papierkorb-Logdatei nicht gefunden"
+    exit 1
+fi
+
+# Iteration über Parameterliste
+for FILE in $@
+do
+    # Zeile in Logdatei finden
+    LINE=$(grep $FILE! $TRASHLOG)
+
+    if [ $? ]
+    then
+	# Ursprungspfad holen und prüfen
+	# Befehl basename
+
+	# Datei wiederherstellen
+
+	# Zeile aus Logdatei entfernen
+	# sed- i ...
+    else
+	echo "$FILE nicht gefunden"
+    fi
+    
+    #echo $LINE
+done
