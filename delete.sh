ab7a223c80b0eabe8e0354991342f466f18c1508
Jonas Posselt <jonas.posselt@web.de>
2018-05-10 14:45:49 +0200

    Löschen von mehreren Dateien auf einmal korrigiert
    Das Verschieben der Dateien erfolgt jetzt in einer Funktion, die in einer Subshell aufgerufen wird. Dadurch erhält jede Verschiebevorgang eine eigene PID.

--- delete.sh
+++ delete.sh
@@ -8,6 +8,29 @@ TRASHLOG="$TRASHDIR/.dir"
 
 #echo $(date '+%y%m%d%H%M%S')
 
+deleteFile() {
+    if [ -e "$1" ]
+    then
+	# Dateihandle anlegen
+	HANDLE=$(date '+%y%m%d%H%M%S')_$BASHPID.dat
+	
+	# Datei verschieben
+	mv "$1" "$TRASHDIR/$HANDLE" 2>/dev/null
+	if [ "$?" -eq "0" ]
+	then	
+		# Eintrag zur Logdatei hinzufügen
+		echo $HANDLE! $(realpath -s "$1") >> "$TRASHLOG"
+
+		# Ausgabe für Benutzer
+		echo $(realpath -s "$1"): "$HANDLE"
+	else 
+	echo Fehler beim Löschen von "$1"
+	fi
+    else
+	echo "$1 existiert nicht"
+    fi
+}
+
 # Parameter prüfen
 if [ -z "$1" ]
 then
@@ -30,23 +53,6 @@ fi
 # Iteration über Parameterliste
 for FILE in "$@"
 do
-    if [ -e "$FILE" ]
-    then
-	# Dateihandle anlegen
-	HANDLE=$(date '+%y%m%d%H%M%S')_$$.dat
-	
-	# Datei verschieben
-	if mv "$FILE" "$TRASHDIR/$HANDLE" 2>/dev/null
-	then	
-		# Eintrag zur Logdatei hinzufügen
-		echo $HANDLE! $(realpath -s "$FILE") >> "$TRASHLOG"
-
-		# Ausgabe für Benutzer
-		echo $(realpath -s "$FILE"): "$HANDLE"
-	else 
-	echo Fehler beim Löschvorgang
-	fi
-    else
-	echo "$FILE existiert nicht"
-    fi
+    # Aufruf Funktion in Subshell
+    (deleteFile "$FILE")
 done
