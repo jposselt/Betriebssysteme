ad58f1d9da99cc5e8fa61a50478aa7c5139d39a6
Jonas Posselt <jonas.posselt@web.de>
2018-06-13 10:57:15 +0200

    Hilfsfunktionen: Datei lesen, Dateiendung bestimmen

---
+++ tools.c
@@ -0,0 +1,58 @@
+//#include <stddef.h>
+//#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "tools.h"
+
+/* Bestimmt die Dateiendung */
+const char *get_filename_ext(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return "";
+    return dot + 1;
+}
+
+/*Hilfsfunktion zum Lesen des Dateiinhalts */
+const char *get_file_content(const char *filename) {
+    char *buffer = "";
+
+    /* Öffne Datei zum Lesen */
+    FILE *infile = fopen(filename, "r");
+
+    /* Abbruch, falls Datei nicht existiert */
+    if (infile == NULL) {
+        perror("Fehler beim Lesen der Datei: ");
+        return buffer;
+    }
+
+    /* Suche Dateiender */
+    if (fseek (infile, 0, SEEK_END)) {
+        perror("Fehler beim Lesen der Datei: ");
+    }
+
+    /* Länge der Datei*/
+    long length = ftell (infile);
+    if (length < 0) {
+        perror("Fehler beim Lesen der Datei: ");
+    }
+
+    /* Zurück zum Anfang */
+    if (fseek (infile, 0, SEEK_SET)) {
+        perror("Fehler beim Lesen der Datei: ");
+    }
+
+    /* Datei einlesen */
+    buffer = malloc (length);
+    if (buffer) {
+        fread (buffer, 1, length, infile);
+    } else {
+        perror("Fehler beim Lesen der Datei: ");
+    }
+
+    /* Datei schließen */
+    fclose(infile);
+
+    /* Rückgabe */
+    return buffer;
+}
---
+++ tools.h
@@ -0,0 +1,11 @@
+//
+// Created by jonas on 13.06.18.
+//
+
+#ifndef SYNCHRONISATION_TOOLS_H
+#define SYNCHRONISATION_TOOLS_H
+
+const char *get_filename_ext(const char *filename);
+const char *get_file_content(const char *filename);
+
+#endif //SYNCHRONISATION_TOOLS_H
