fbfdd03f6f8fedbeb4aa43177e54c6e76cd2efb9
Jonas Posselt <jonas.posselt@web.de>
2018-06-19 21:45:48 +0200

    Schreiben absichern

--- tools.c
+++ tools.c
@@ -6,7 +6,7 @@
 
 #include "tools.h"
 
-#define BUFFER_SIZE 2
+#define BUFFER_SIZE 20
 #define COPY_BYTES 10
 
 /* Write an error message to stderr */
@@ -23,6 +23,19 @@ off_t getFileSize(const int fd) {
     } else {
         return fileStat.st_size;
     }
+}
+
+/* Write bytes from a buffer */
+int writeFromBuffer(int fd, char* buffer, size_t nBytes) {
+    while(nBytes > 0) {
+        ssize_t written = write(fd, buffer, nBytes);
+        if (written < 0) {
+            perror("writeFromBuffer: ");
+            return -1;
+        }
+        nBytes -= written;
+    }
+    return 0;
 }
 
 /* Print second then first half of file to stdout */
@@ -49,8 +62,8 @@ void printHalfHalf(const int fd) {
             perror("printHalfHalf: ");
             return;
         }
-        if (write(STDOUT, buffer, (size_t)nRead) < 0) {
-            perror("printHalfHalf: ");
+        if ( writeFromBuffer(STDOUT, buffer, (size_t) nRead) < 0) {
+            writeError("printHalfHalf: Could not write to stdout\n");
             return;
         }
         count -= nRead;
@@ -68,8 +81,8 @@ void printHalfHalf(const int fd) {
             perror("printHalfHalf: ");
             return;
         }
-        if (write(STDOUT, buffer, (size_t)nRead) < 0) {
-            perror("printHalfHalf: ");
+        if ( writeFromBuffer(STDOUT, buffer, (size_t) nRead) < 0) {
+            writeError("printHalfHalf: Could not write to stdout\n");
             return;
         }
         count -= nRead;
@@ -125,7 +138,7 @@ void printFile(const int src, const int dest) {
 
 void fileInsert(const int src, const int dest) {
     long int size = getFileSize(src);
-    long int bytesToMove = MAX(size - COPY_BYTES, 0);
+    long int totalBytesToMove = MAX(size - COPY_BYTES, 0);
     long int insertPosition = MIN( MAX(size - 1, 0), COPY_BYTES);
 
     /* Get last COPY_BYTES characters from src */
@@ -140,8 +153,8 @@ void fileInsert(const int src, const int dest) {
         return;
     }
 
-    /* Insert */
-    if (bytesToMove == 0) {
+    /* Insert for files shorter than COPY_BYTES */
+    if (totalBytesToMove == 0) {
         if (lseek(dest, insertPosition, SEEK_SET) < 0) {
             perror("fileInsert: ");
             return;
@@ -151,41 +164,38 @@ void fileInsert(const int src, const int dest) {
             return;
         }
     } else {
-        /* Move end bytes */
+        /* Insert with moving of end bytes */
         char moveBuffer[BUFFER_SIZE];
-        while (bytesToMove > 0) {
+        while (totalBytesToMove > 0) {
             /* Calculate and set the position from there to read */
-            long int readPosition = insertPosition + bytesToMove - MIN(BUFFER_SIZE, bytesToMove);
+            long int readPosition = insertPosition + totalBytesToMove - MIN(BUFFER_SIZE, totalBytesToMove);
             if (lseek(dest, readPosition, SEEK_SET) < 0) {
                 writeError("fileInsert: Could not set destination read position\n");
                 perror("fileInsert: ");
                 return;
             }
-            ssize_t movingBytes = read(dest, moveBuffer, (size_t) MIN(BUFFER_SIZE, bytesToMove));
+            ssize_t movingBytes = read(dest, moveBuffer, (size_t) MIN(BUFFER_SIZE, totalBytesToMove));
             if( movingBytes < 0 ) {
                 writeError("fileInsert: Could not read bytes for moving\n");
                 perror("fileInsert: ");
                 return;
             }
 
-            /* Write the bytes to their new position */
+            /* Shift the bytes to their new position */
             long int writePosition = readPosition + COPY_BYTES;
             if (lseek(dest, writePosition, SEEK_SET) < 0) {
                 writeError("fileInsert: Could not set destination write position\n");
                 perror("fileInsert: ");
                 return;
             }
-            long int bytesToWrite = movingBytes;
-            while (bytesToWrite > 0) {
-                ssize_t actualWritten = write(dest, moveBuffer, (size_t) bytesToWrite);
-                if (actualWritten < 0) {
-                    writeError("fileInsert: Could not write bytes for moving\n");
-                    perror("fileInsert: ");
-                    return;
-                }
-                bytesToWrite -= actualWritten;
+
+            if ( writeFromBuffer(dest, moveBuffer, (size_t) movingBytes) < 0) {
+                writeError("fileInsert: Could not write bytes for moving\n");
+                return;
             }
-            bytesToMove -= movingBytes;
+
+            /* Update counter */
+            totalBytesToMove -= movingBytes;
         }
 
         /* Insert new bytes */
@@ -194,14 +204,9 @@ void fileInsert(const int src, const int dest) {
             perror("fileInsert: ");
             return;
         }
-        while(actualCopy) {
-            ssize_t actualWritten = write(dest, insertBuffer, (size_t) actualCopy);
-            if (actualWritten < 0) {
-                writeError("fileInsert: Could not write bytes for inserting\n");
-                perror("fileInsert: ");
-                return;
-            }
-            actualCopy -= actualWritten;
+        if ( writeFromBuffer(dest, insertBuffer, (size_t) actualCopy) < 0) {
+            writeError("fileInsert: Could not write bytes for inserting\n");
+            return;
         }
     }
 
